d30f3b77a27a5f7af91f83f3b9cf684ff7f566a5
sepi199 <pepesifre@gmail.com>
2016-05-03 21:58:10 +0500

    Empezamos con email confirmacion

---
+++ Manteca-Box-develop/ConfirmacionRegistro.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Manteca_Box_develop
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString.Count > 0)
+            {
+                if (Request.QueryString.Keys[0] == "email")
+                {
+                    string email = Request.QueryString["email"].ToString();
+                    string QueryActivate = "Update Users set verified = '1' where email = '" + email + "'";
+
+                    //LLamar a un en o cad con esta sentencia, para que modifique la columna verified
+                }
+            }
+        }
+    }
+}
